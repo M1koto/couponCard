512aedd56b2b6020842478cdb621e5c395c77adf
M1koto <yenming891109@gmail.com>
2022-05-25 23:09:20 -0700

    add: clickable button to desire stamp img

--- popup.js
+++ popup.js
@@ -1,7 +1,30 @@
 window.addEventListener('DOMContentLoaded', function () {
+    addClickable();
+});
+
+function addClickable() {
+    elements = document.getElementsByClassName("button");
+    Array.prototype.forEach.call(elements, function(element) {
+        element.addEventListener("click", function() {
+           replacePic(element);
+        });
+     });
+    // for (let i = 1; i < 11; i++) {
+    //     but = "button".concat(i.toString())
+    //     ele = document.getElementsByClassName(but)[0]
+    //     ele.addEventListener("click", function() {
+    //         console.log("f");
+    //         replacePic(ele);
+    //     });
+    // }
+}
+
+function replacePic(ele) {
     var img = new Image()
     img.className = "stamp"
     img.src = "./images/stamp.jpeg"
-    s1 = document.getElementsByClassName('button1')[0];
-    s1.parentNode.replaceChild(img, s1)
-});
+    ele.parentNode.replaceChild(img, ele)
+    // console.log(ele.className);
+    
+    // ele.innerHTML = '<input type="image" class="stamp" src="./images/stamp.jpeg" />'
+}
