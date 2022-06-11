c58f456c84883e3103226ee5390d2687ed0c267e
M1koto <yenming891109@gmail.com>
2022-06-10 18:25:16 -0700

    add: persistent when repress icon

--- popup.js
+++ popup.js
@@ -1,28 +1,46 @@
+
 window.addEventListener('DOMContentLoaded', function () {
-    elements = document.getElementsByClassName("button");
-    Array.prototype.forEach.call(elements, function(element) {
-        addClickable(element);
-     });
+    if (localStorage.getItem("first") == null) {
+        localStorage.clear()
+        s = "button"
+        for (let i=1; i<11; i++) {
+            temp = s + i.toString();
+            localStorage.setItem(temp, false)
+        }
+        localStorage.setItem("first", false);
+    } else {
+        elements = document.getElementsByClassName("button");
+        Array.prototype.forEach.call(elements, function(element) {
+            val = localStorage.getItem(element.className.substring(7))
+            if (val == "true") { // yes stamp
+                replaceWithPic(element)
+            } else {
+                addClickable(element)
+            }
+        });
+    }
 });
 
 function addClickable(element) {
     element.addEventListener("click", function() {
-        replacePic(element);
+        localStorage.setItem(element.className.substring(7), true)
+        replaceWithPic(element);
      });
 }
 
-function replacePic(ele) {
+function replaceWithPic(ele) {
     var img = new Image()
     img.src = "./images/stamp.jpeg"
     img.className = ele.className;
     img.addEventListener("click", function() {
+        localStorage.setItem(ele.className.substring(7), false)
         cancelPic(img.className);
     });
     ele.parentNode.replaceChild(img, ele);
 }
 
 function cancelPic(stampName) {
-    stamp = document.getElementsByClassName(stampName.substring(6))[0];
+    stamp = document.getElementsByClassName(stampName.substring(7))[0];
     btn = document.createElement("button");
     btn.className = stampName;
     addClickable(btn);
