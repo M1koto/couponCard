d50eab9606c7fb7fed38220f90c5aa653c1edc94
M1koto <yenming891109@gmail.com>
2022-06-14 16:29:22 -0700

    add: stamp animation and logic; final stamp image;

--- popup.js
+++ popup.js
@@ -1,9 +1,14 @@
 
 window.addEventListener('DOMContentLoaded', function () {
+    // document.getElementsByClassName("card-title").title = "fuck"
+    // // title.addEventListener("click", function() {
+    // //     title.title = "fuck"
+    // // })
+
     if (localStorage.getItem("first") == null) {
         localStorage.clear()
         s = "button"
-        for (let i=1; i<11; i++) {
+        for (let i=0; i<10; i++) {
             temp = s + i.toString();
             localStorage.setItem(temp, false)
         }
@@ -13,7 +18,7 @@ window.addEventListener('DOMContentLoaded', function () {
         Array.prototype.forEach.call(elements, function(element) {
             val = localStorage.getItem(element.className.substring(7))
             if (val == "true") { // yes stamp
-                replaceWithPic(element)
+                initWithPic(element)
             } else {
                 addClickable(element)
             }
@@ -28,10 +33,22 @@ function addClickable(element) {
      });
 }
 
+function initWithPic(ele) {
+    var img = new Image()
+    img.src = "./images/stamp.png"
+    img.className = ele.className;
+    img.addEventListener("click", function() {
+        localStorage.setItem(ele.className.substring(7), false)
+        cancelPic(img.className);
+    });
+    ele.parentNode.replaceChild(img, ele);
+}
+
 function replaceWithPic(ele) {
     var img = new Image()
-    img.src = "./images/stamp.jpeg"
+    img.src = "./images/stamp.png"
     img.className = ele.className;
+    img.className += " stamp"
     img.addEventListener("click", function() {
         localStorage.setItem(ele.className.substring(7), false)
         cancelPic(img.className);
@@ -42,7 +59,7 @@ function replaceWithPic(ele) {
 function cancelPic(stampName) {
     stamp = document.getElementsByClassName(stampName.substring(7))[0];
     btn = document.createElement("button");
-    btn.className = stampName;
+    btn.className = stampName.substring(0, 10);
     addClickable(btn);
     stamp.parentNode.replaceChild(btn, stamp);
 }
